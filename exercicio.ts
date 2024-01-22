cde441b8c42746833eb2dd1979d10e5ba777b9d1
Ana Claudia <139407636+anameneiro@users.noreply.github.com>
2024-01-22 19:08:51 -0300

    add exercicio ts

---
+++ exercicio.ts
@@ -0,0 +1,12 @@
+
+function multiplicar(a: number, b: number): number {
+  return a * b;
+}
+
+
+function saudacao(nome: string): string {
+  return `Olá ${nome}`;
+}
+
+console.log(multiplicar(7, 66)); 
+console.log(saudacao('Ana'));
